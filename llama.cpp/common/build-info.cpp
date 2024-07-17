9456364a69d26ba613ebd50bf83b057e0a00d1b1
D0112 <97210622+D0112@users.noreply.github.com>
2024-07-17 10:22:24 +0800

    Add files via upload

---
+++ llama.cpp/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 3350;
+char const *LLAMA_COMMIT = "7fdb6f73";
+char const *LLAMA_COMPILER = "cc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0";
+char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
