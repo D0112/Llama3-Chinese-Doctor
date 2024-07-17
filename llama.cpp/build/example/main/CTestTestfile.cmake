05e3310ea5b3d16ad90fd9c83c51049ef8300d6f
D0112 <97210622+D0112@users.noreply.github.com>
2024-07-17 10:48:30 +0800

    Add files via upload

---
+++ llama.cpp/build/example/main/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/adminn/Desktop/llamacpp/llama.cpp/examples/main
+# Build directory: /home/adminn/Desktop/llamacpp/llama.cpp/build/examples/main
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
